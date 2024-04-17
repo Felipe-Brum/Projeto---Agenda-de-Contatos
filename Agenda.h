c1466273bdc979c4ab4c66094d2ed3f6797eb039
Felipe Brum Pereira <132711836+Felipe-Brum@users.noreply.github.com>
2024-04-17 03:35:38 +0000

    Funções para adicionar, listar e deletar contatos

--- Agenda.h
+++ Agenda.h
@@ -4,7 +4,7 @@
 #define tamanho_email 30
 #define TOTAL 255
 
-typedef enum {OK, MAX_TAREFAS, SEM_CONTATOS, NOME_INVALIDO, SOBRENOME_INVALIDO, EMAIL_INVALIDO, TELEFONE_INVALIDO} ERROS;
+typedef enum {OK, MAX_TAREFAS, TELEFONE_NAO_ENCONTRADO, SEM_CONTATOS, NOME_INVALIDO, SOBRENOME_INVALIDO, EMAIL_INVALIDO, TELEFONE_INVALIDO} ERROS;
 
 
 typedef struct {
@@ -18,6 +18,7 @@ typedef ERROS (*funcao)(Contato[], int*);
 
 ERROS adicionar(Contato contato[], int *pos);
 ERROS listar(Contato contato[], int *pos);
+ERROS deletar(Contato contato[], int *pos);
 
 
 void clearBuffer();
